ee5332e2e76563bda0d47a599b58dda66dafd782
Laura Sofia Dueñas Bulla <laura.duenas@mercadolibre.com.co>
2023-09-13 11:12:39 -0500

    feat: R3 Service methods

--- internal/tickets/service.go
+++ internal/tickets/service.go
@@ -1,1 +1,45 @@
 package tickets
+
+import (
+	"context"
+	"errors"
+)
+
+type ticketService struct{
+	repo Repository
+}
+
+type serviceController interface {
+	GetTotalTickets(ctx context.Context,  destination string) (int, error)
+	AverageDestination(ctx context.Context,  destination string) (float64, error)
+}
+
+func NewService (repository Repository) ticketService {
+	return ticketService{repo: repository}
+}
+
+func (ts *ticketService) GetTotalTickets(ctx context.Context,  destination string) (total int, er error){
+	ticketsList, err := ts.repo.GetTicketByDestination(ctx, destination)
+	if err != nil {
+		er = errors.New("Can't get tickets list")
+	}
+	total = len(ticketsList)
+	return 
+}
+
+func (ts ticketService) AverageDestination(ctx context.Context,  destination string) (average float64, er error) {
+	totalTicketsListPerDestination, err := ts.repo.GetTicketByDestination(ctx, destination)
+	if err != nil {
+		er = errors.New("Can't get tickets list per destination")
+	}
+	totalTicketsPerDestination := len(totalTicketsListPerDestination)
+
+	ticketsList, err := ts.repo.GetAll(ctx)
+	if err != nil {
+		er = errors.New("Can't get tickets list")
+	}
+	totalTickets := len(ticketsList)
+
+	average = float64(totalTicketsPerDestination / totalTickets)
+	return
+}
